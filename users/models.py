c5d8a643f71558e0e67942834079ea968eca14c0
pahanova <pahan470@outlook.com>
2021-10-10 00:54:19 +0300

    новые исправленные миграции, модель юзера

---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    """
+    Пользователь
+    """
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    username = None
+    email = models.EmailField(unique=True)
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
